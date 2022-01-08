5fd2a944be1033067c62ba65806f94bac9f15ce8
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-08 06:11:14 -0500

    Add testing suite

---
+++ src/components/Dashboard.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import TestRenderer from 'react-test-renderer';
+import store from '../redux/configureStore';
+import * as dashboardSlice from '../redux/dashboard/dashboard';
+import Dashboard from './Dashboard';
+
+describe('App Rendering', () => {
+  test('Render dashboard component', () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Dashboard />
+        </Provider>
+      </BrowserRouter>,
+    );
+    screen.debug();
+  });
+  test('Dashboard data loads properly', () => {
+    const tree = TestRenderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Dashboard />
+        </Provider>
+      </BrowserRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
