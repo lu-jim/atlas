e7531b08099881fb41af30b6e99fd0b9c7d6ffdb
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-08 07:31:27 -0500

    Improve navbar styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,18 @@ import { Link, useLocation } from 'react-router-dom';
 const Header = () => {
   const isHome = useLocation();
   const backButtonVisibility = isHome.pathname === '/' ? 'hidden' : 'flex';
-  const navStyle = `${backButtonVisibility} bg-indigo-900 text-white justify-between p-6 text-center align-center h-10`;
+  const navStyle = `${backButtonVisibility} bg-sky-900 text-white justify-between p-6 align-baseline w-screen`;
   return (
     <nav id="navbar" className={navStyle}>
-      <div className="flex gap-10 items-center">
-        <Link to="/"><h1 className="text-white text-3xl font-bold display-mono"> &lt; </h1></Link>
-        <ul className="gap-8 flex self-center">
-          <li className="hover:text-orange-300 uppercase tracking-wide ">
-            <Link to="/">Atlas</Link>
-          </li>
-          <li className="hover:text-orange-300 uppercase tracking-wide ">
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </div>
+      <Link to="/" className="self-start w-60"><h1 className="text-white text-3xl font-bold display-mono"> &lt; </h1></Link>
+      <ul className="gap-8 flex self-center">
+        <li className="hover:text-orange-300 uppercase tracking-wide ">
+          <Link to="/">Atlas</Link>
+        </li>
+        <li className="hover:text-orange-300 uppercase tracking-wide ">
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
