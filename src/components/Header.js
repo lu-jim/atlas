842567b4ebee8156e3dd4248581a8ca90dd9cc94
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-08 07:13:43 -0500

    Update navbar to be shown when leaving home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Header = () => (
-  <nav id="navbar" className="bg-white text-slate-500 flex justify-between p-6 text-center align-center h-10">
-    <div className="flex gap-10 items-center">
-      <h1 className="text-blue-600 text-3xl font-bold"> Atlas </h1>
-      <ul className="flex justify-evenly gap-8">
-        <li className="hover:text-orange-300 uppercase tracking-wide">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="hover:text-orange-300 uppercase tracking-wide ">
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+const Header = () => {
+  const isHome = useLocation();
+  const backButtonVisibility = isHome.pathname === '/' ? 'hidden' : 'flex';
+  const navStyle = `${backButtonVisibility} bg-indigo-900 text-white justify-between p-6 text-center align-center h-10`;
+  return (
+    <nav id="navbar" className={navStyle}>
+      <div className="flex gap-10 items-center">
+        <Link to="/"><h1 className="text-white text-3xl font-bold display-mono"> &lt; </h1></Link>
+        <ul className="gap-8 flex self-center">
+          <li className="hover:text-orange-300 uppercase tracking-wide ">
+            <Link to="/">Atlas</Link>
+          </li>
+          <li className="hover:text-orange-300 uppercase tracking-wide ">
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
